207dd0b5223203945e3767a4c855ff4ca14c59e0
Kashif Roshen <kashif@wisenet.co>
2019-12-09 10:12:38 +0530

    implemented a sleep function using promise constructor

---
+++ sleep.js
@@ -0,0 +1,19 @@
+function sleep(ms) {
+  return new Promise((resolve) => {
+    //  throw Error('Unexpected Error occured');
+
+    setTimeout(resolve, ms);
+  });
+}
+
+// usage example
+console.log('start');
+
+sleep(1000)
+  .then(() => {
+    console.log(' one sec passed');
+
+    return sleep(1000);
+  })
+  .then(() => console.log('two sec passed'))
+  .catch((error) => console.error(error));
